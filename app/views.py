733e1fe3f1980db4b9c62f83494b0e321318173c
Kinga Kruppa <kruppa.kinga@gmail.com>
2017-07-25 19:33:37 +0200

    Add hotel id

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from flask import jsonify
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import render_template, flash, redirect, session, url_for, request, make_response, abort
 from app import models, db
 from flask_bootstrap import Bootstrap
 
@@ -42,7 +42,8 @@ def get_hotel(hotel_id):
 					 'photos_url' : url_for('get_hotel_photos', hotel_id=h.code, _external=True),
 					 'reviews_breakdown' : url_for('get_hotel_review_breakdown', hotel_id=h.code, _external=True),
 					 'main_photo' : h.main_pic,
-					 'hotel_url' : h.booking_url
+					 'hotel_url' : h.booking_url,
+					 'hotel_id' : h.code
 
 	} )
 	return jsonify(  result  )
@@ -102,6 +103,10 @@ def compare():
 			checkin = value
 		else:
 			hotel = models.Hotel.query.filter(models.Hotel.code == value).first()
+			hotel_data = json.loads(get_hotel(value).data)
+			# if len(hotel_data) < 1:
+			# 	abort(500)
+
 			static_data = json.loads(get_hotel(value).data)[0]
 
 			price = get_available_hotel(checkin, checkout, hotel.code) 
@@ -117,8 +122,18 @@ def get_available_hotel( checkin, checkout, hotel_id):
 	url = 'https://distribution-xml.booking.com/json/getHotelAvailabilityV2?checkin={}&checkout={}&room1=A,A&output=room_details,hotel_details&hotel_ids={}'.format(checkin, checkout,  hotel_id)
 	print (url)
 	hotel = requests.get(url, auth=(user, pwd)).json()
+	# if len(hotel['hotels']) == 0:
+	# 	abort(500)
+
 	return {'booking_url' : hotel['hotels'][0]['hotel_url'], 
 			'room_min_price' : hotel['hotels'][0]['price'], 
 			'breakfast_included' : hotel['hotels'][0]['room_min_price']['breakfast_included'],
 			'currency' : hotel['hotels'][0]['hotel_currency_code']
 			}	
+
+
+# @app.errorhandler(500)
+# def not_found(error):
+# 	return make_response(jsonify({'error': 'Hotel is not available in that time interval'}), 500)
+
+
--- create_initial_db.py
+++ create_initial_db.py
@@ -7,13 +7,12 @@ from secret import user, pwd
 from app import db, models
 
 
-def get_cities():
+def get_data():
 	limit_rows = '1000'
-	base_url = 'https://distribution-xml.booking.com/json/bookings.getCities?countrycodes=nl&languagecodes=en&rows=' + limit_rows + '&offset='
-	url = base_url
+	baseurl = 'https://distribution-xml.booking.com/json/bookings.'
+	url = baseurl + 'getCities?countrycodes=nl&languagecodes=en&city_ids=-2140479'
 	result = []
-	i = 0
-	cities = requests.get(url + '0&city_ids=-2140479', auth=(user, pwd)).json()
+	cities = requests.get(url, auth=(user, pwd)).json()
 	
 	for city in cities:
 		c = models.City(name=city['name'], code = city['city_id'])
@@ -21,7 +20,7 @@ def get_cities():
 		db.session.commit()
 
 		ts = time.time()
-		url = 'https://distribution-xml.booking.com/json/bookings.getHotels?city_ids={}&languagecodes=en&rows=15'.format(city['city_id'])
+		url = baseurl + 'getHotels?city_ids={}&languagecodes=en&rows=15'.format(city['city_id'])
 		hotels = requests.get(url, auth=(user, pwd)).json()
 		for hotel in hotels:
 			h = models.Hotel(name=hotel['name'], code = hotel['hotel_id'], address = hotel['address'], url = hotel['url'],  
@@ -30,27 +29,27 @@ def get_cities():
 				 city = c)
 			db.session.add(h)
 
-			url = 'https://distribution-xml.booking.com/json/bookings.getHotelDescriptionPhotos?hotel_ids={}'.format(hotel['hotel_id'])
+			url = baseurl + 'getHotelDescriptionPhotos?hotel_ids={}'.format(hotel['hotel_id'])
 			pics = requests.get(url, auth=(user, pwd)).json()
 			for pic in pics:
 				p = models.Picture(url=pic['url_original'], url_max_300 = pic['url_max300'], code=pic['photo_id'], hotel = h)
 				db.session.add(p)
 
-			url = 'https://distribution-xml.booking.com/json/bookings.getBookingcomReviews?hotel_ids={}&languagecodes=en'.format(hotel['hotel_id'])
+			url = baseurl + 'getBookingcomReviews?hotel_ids={}&languagecodes=en'.format(hotel['hotel_id'])
 			reviews = requests.get(url, auth=(user, pwd)).json()
 			for review in reviews:
 				r = models.Review(pro=review['pros'], con = review['cons'], headline=review['headline'], avg_score=review['average_score'], hotel = h)
 				db.session.add(r)
 
-			url = 'https://distribution-xml.booking.com/json/bookings.getHotelPhotos?hotel_ids={}'.format(hotel['hotel_id'])
+			url = baseurl + 'getHotelPhotos?hotel_ids={}'.format(hotel['hotel_id'])
 			main_pic = requests.get(url, auth=(user, pwd)).json()
 			h.main_pic = pic['url_max300']
 
-			url = 'https://distribution-xml.booking.com/json/bookings.getHotelDescriptionTranslations?hotel_ids={}&languagecodes=en'.format(hotel['hotel_id'])
+			url = baseurl + 'getHotelDescriptionTranslations?hotel_ids={}&languagecodes=en'.format(hotel['hotel_id'])
 			descr = requests.get(url, auth=(user, pwd)).json()
 			h.desc = descr[0]['description']
 
-			url = 'https://distribution-xml.booking.com/json/bookings.getBookingcomReviewScores?hotel_ids={}'.format(hotel['hotel_id'])
+			url = baseurl + 'getBookingcomReviewScores?hotel_ids={}'.format(hotel['hotel_id'])
 			scores = requests.get(url, auth=(user, pwd)).json()
 			for sc in scores:
 				breakdown = sc['score_breakdown']
@@ -86,4 +85,4 @@ def get_cities():
 
 	
 
-get_cities()
+get_data()
