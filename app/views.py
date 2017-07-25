7c1b3c7c4d534b6eb421d3d01f10f6aec35756f7
Kinga Kruppa <kruppa.kinga@gmail.com>
2017-07-25 12:41:15 +0200

    Simple comparison added

--- app/views.py
+++ app/views.py
@@ -19,36 +19,31 @@ def get_hotels():
 	result = []
 	for h in hotels:
 		result.append( {'name' : h.name ,
-						'url' : url_for('get_hotel', hotel_id=h.code, _external=True),
-						'review_url' : url_for('get_hotel_reviews', hotel_id=h.code, _external=True),
-						'photos_url' : url_for('get_hotel_photos', hotel_id=h.code, _external=True),
-
+						'code' : h.code ,
+						'url' : url_for('get_hotel', hotel_id=h.code, _external=True)
 						}
 			)
 	return jsonify(  result )
 
 @app.route('/hotels/<string:hotel_id>', methods=['GET'])
 def get_hotel(hotel_id):
-	hotels = models.Hotel.query.all()
-	hotel = [h for h in hotels if h.code == hotel_id]
-	if len(hotel)!=1:
-		abort(404)
-	h = hotel[0]
+	h = models.Hotel.query.filter(models.Hotel.code == hotel_id).first()
 	result = []
 	result.append( { 'name' : h.name ,
 					 'review_score': h.review_score,
+					 'location' : { 'latitude'  : h.latitude, 'longitude'  : h.longitude },
+					 'reviews_url' : url_for('get_hotel_reviews', hotel_id=h.code, _external=True),
+					 'photos_url' : url_for('get_hotel_photos', hotel_id=h.code, _external=True),
+
 	} )
 	return jsonify(  result  )
 
 
 @app.route('/hotels/reviews/<string:hotel_id>', methods=['GET'])
 def get_hotel_reviews(hotel_id):
-	hotels = models.Hotel.query.all()
-	hotel = [h for h in hotels if h.code == hotel_id]
-	if len(hotel)!=1:
-		abort(404)
+	hotel = models.Hotel.query.filter(models.Hotel.code == hotel_id).first()
 	rev_result = []
-	for r in hotel[0].reviews:
+	for r in hotel.reviews:
 		rev_result.append ( { 'headline' : r.headline ,
 							  'pro' : r.pro,
 							  'con' : r.con
@@ -58,14 +53,23 @@ def get_hotel_reviews(hotel_id):
 
 @app.route('/hotels/photos/<string:hotel_id>', methods=['GET'])
 def get_hotel_photos(hotel_id):
-	hotels = models.Hotel.query.all()
-	hotel = [h for h in hotels if h.code == hotel_id]
-	if len(hotel)!=1:
-		abort(404)
+	hotel = models.Hotel.query.filter(models.Hotel.code == hotel_id).first()
 	pics_result = []
-	for pic in hotel[0].pics:
+	for pic in hotel.pics:
 		pics_result.append ( { 'url' : pic.url ,
-							  'url_max_300' : pic.url_max_300
+							   'url_max_300' : pic.url_max_300
 							} )
 	
 	return jsonify(  pics_result  )
+
+@app.route('/compare', methods=['GET'])
+def compare():
+	result = []
+	hotels = request.args.to_dict()
+	for key, hotel_id in hotels.items():
+		hotel = models.Hotel.query.filter(models.Hotel.code == hotel_id).first()
+		result.append ( json.loads(get_hotel(hotel_id).data)[0] )
+	print ( result )
+	return jsonify( result )
+
+
