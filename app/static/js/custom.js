0d266e406756f17d208b1a2da55d96cd4614e62e
Jeanna Hamilton <hamilton.jeanna@gmail.com>
2017-07-26 00:09:31 +0200

    Basis for shelf open and closing

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -23,6 +23,34 @@ function loadView() {
 
     console.log('inside loadView');
     $('[data-toggle="tooltip"]').tooltip();
+
+    //Navigation Menu Slider
+    $('#nav-expander').on('click',function(e){
+      e.preventDefault();
+      $('body').toggleClass('nav-expanded');
+    });
+    $('#nav-close').on('click',function(e){
+      e.preventDefault();
+      $('body').removeClass('nav-expanded');
+    });
+
+
+    // Initialize navgoco with default options
+    $(".main-menu").navgoco({
+        caret: '<span class="caret"></span>',
+        accordion: false,
+        openClass: 'open',
+        save: true,
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 300,
+            easing: 'swing'
+        }
+    });
 };
 
 function clickHandler() {
